4b8fdd614e3068ad4d7f81df6992a7ac2e9be93b
Guilherme Yoshida Teixeira <contact@gyt.app>
2023-01-14 17:03:41 -0300

    :sparkles: Feat: Tests for Entities and Exceptions

---
+++ tests/exceptions/test_item_not_found_error.py
@@ -0,0 +1,10 @@
+import unittest
+
+from src.exceptions.item_not_found_error import ItemNotFoundError
+
+
+class TestItemNotFoundError(unittest.TestCase):
+
+    def test_item_not_found_error(self):
+        with self.assertRaises(ItemNotFoundError):
+            raise ItemNotFoundError

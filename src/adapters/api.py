1703ddc39fbb8e873ab625d85a1e63b119685477
Guilherme Yoshida Teixeira <contact@gyt.app>
2023-01-14 16:59:53 -0300

    :recycle: Fix: Remove Async

--- src/adapters/api.py
+++ src/adapters/api.py
@@ -9,15 +9,15 @@ router = APIRouter()
 
 
 @router.get("/items/{item_id}")
-async def read_item(item_id: int, service: ItemUseCases = Depends(item_service)):
+def read_item(item_id: int, service: ItemUseCases = Depends(item_service)):
     try:
         item = service.get_item(item_id)
-        return item
     except ItemNotFoundError as e:
         raise HTTPException(status_code=404, detail=str(e))
+    return item
 
 
 @router.post("/items/")
-async def create_item(item: Item, service: ItemUseCases = Depends(item_service)):
+def create_item(item: Item, service: ItemUseCases = Depends(item_service)):
     new_item = service.create_item(item.dict())
     return new_item
